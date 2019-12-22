d046b6703ab8e52028f0473ecf2868e84ebc6544
elsie <el.d.sieglova@seznam.cz>
2019-12-22 02:18:24 +0100

    odstranit printy pro kontrolu, uprava komentaru

--- split.py
+++ split.py
@@ -16,7 +16,6 @@ def convert_to_numbers(list):
         float(i)
     return
 
-# musime spojit data a zapsat
 def append_clusterIDs(data, clusterIDs):
     """appends clusterIDs to coordinates"""
     for feature in data['features']:
@@ -31,15 +30,12 @@ with open("input.geojson", "r", encoding="utf-8") as f:
 
 # list of coordinates converted to float
 coordinates = load_coordinates(data)
-#coordinates = convert_to_numbers(list_of_coordinates)
 
 # turtle draw coordinates
-test_list = [(14.4464198, 50.0790449),(1,1),(0,1),(1,100)]
 draw_coords(coordinates)
 
 # returns clusterID for coordinates
 clusterIDs = build_quadtree(coordinates)
-print(clusterIDs)
 
 # append cluster ID to input data
 new_data = append_clusterIDs(data,clusterIDs)
